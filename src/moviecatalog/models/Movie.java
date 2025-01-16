29634df3809e269ec4444d4db2318d7d71e5a0cd
natanael <rojonatanael99@gmail.com>
2025-01-15 21:24:46 -0400

    feature(Creating Movie model): Implementing a basic Movie model

---
+++ src/moviecatalog/models/Movie.java
@@ -0,0 +1,72 @@
+package moviecatalog.models;
+
+import java.io.Serializable;
+
+public class Movie implements Serializable {
+
+    private String title;
+    private String description;
+    private int rating;
+
+    public Movie() {
+        // Empty constructor
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((title == null) ? 0 : title.hashCode());
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        result = prime * result + rating;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Movie other = (Movie) obj;
+        if (title == null) {
+            if (other.title != null)
+                return false;
+        } else if (!title.equals(other.title))
+            return false;
+        if (description == null) {
+            if (other.description != null)
+                return false;
+        } else if (!description.equals(other.description))
+            return false;
+        if (rating != other.rating)
+            return false;
+        return true;
+    }
+}
